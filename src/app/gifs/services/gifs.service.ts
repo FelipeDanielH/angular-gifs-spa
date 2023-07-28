7115881cdddfdec10b05df6dde89de5ed17b9acf
Felipe <felipe.daniel.henriquez@gmail.com>
2023-07-27 20:13:10 -0400

    se agrega el tipo de dato a una respuesta

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,28 +1,44 @@
-import { Injectable } from '@angular/core';
+import { SearchResponse } from './../interfaces/gifs.interfaces'
+import { Injectable } from '@angular/core'
+import { HttpClient,HttpParams } from '@angular/common/http'
 
-@Injectable({providedIn: 'root'})
+
+@Injectable({ providedIn: 'root' })
 export class GifsService {
 
   private _tagsHistory: string[] = []
   private apiKey: string = 'Deq3UeMV1h0L2eVWh8TIBsFpMQ4yTMmi'
+  private serviceUrl:string ='https://api.giphy.com/v1/gifs'
 
-  get tagsHistory():string[] {
+  constructor( private http: HttpClient){}
+  get tagsHistory(): string[] {
     return [...this._tagsHistory];
   }
 
-  private organizeHistory(tag: string){
+  private organizeHistory(tag: string) {
     tag = tag.toLowerCase();
 
-    if( this._tagsHistory.includes(tag)){
-      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag)
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag)
     }
 
     this._tagsHistory.unshift(tag);
-    this._tagsHistory = this.tagsHistory.splice(0,10);
+    this._tagsHistory = this.tagsHistory.splice(0, 10)
   }
 
-  searchTag(tag: string):void {
+  searchTag(tag: string): void {
     if (tag.length === 0) return
-    this.organizeHistory(tag);
+    this.organizeHistory(tag)
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey )
+      .set('limit', '10' )
+      .set('q', tag)
+      .set('api_key', this.apiKey )
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`,{params})
+    .subscribe( resp => {
+      console.log(resp.data);
+    })
   }
 }
